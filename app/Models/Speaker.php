2a6d67337e0016d91b2c6971494d9665b42bc57d
Luke Armstrong <306764+lukearmstrong@users.noreply.github.com>
2024-10-22 09:34:27 +0100

    forms

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Filament\Forms;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -17,5 +18,46 @@ class Speaker extends Model
     public function conferences(): BelongsToMany
     {
         return $this->belongsToMany(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            Forms\Components\TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('email')
+                ->email()
+                ->required()
+                ->maxLength(255),
+            Forms\Components\Textarea::make('bio')
+                ->required()
+                ->columnSpanFull(),
+            Forms\Components\TextInput::make('twitter_handle')
+                ->maxLength(255),
+            Forms\Components\CheckboxList::make('qualifications')
+                ->columnSpanFull()
+                ->searchable()
+                ->bulkToggleable()
+                ->options(
+                    [
+                        'business-leader' => 'Business Leader',
+                        'charisma' => 'Charismatic Speaker',
+                        'first-time' => 'First Time Speaker',
+                        'hometown-hero' => 'Hometown Hero',
+                        'humanitarian' => 'Works in Humanitarian Field',
+                        'laracasts-contributor' => 'Laracasts Contributor',
+                        'twitter-influencer' => 'Large Twitter Following',
+                        'youtube-influencer' => 'Large YouTube Following',
+                        'open-source' => 'Open Source Creator / Maintainer',
+                        'unique-perspective' => 'Unique Perspective'
+                    ]
+                )
+                ->descriptions([
+                    'business-leader' => 'Here is a nice long description',
+                    'charisma' => 'This is even more information about why you should pick this one',
+                ])
+                ->columns(3),
+        ];
     }
 }
--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Enums\Region;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -17,5 +20,26 @@ class Venue extends Model
     public function conferences(): HasMany
     {
         return $this->hasMany(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('city')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('country')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('postal_code')
+                ->required()
+                ->maxLength(255),
+            Select::make('region')
+                ->enum(Region::class)
+                ->options(Region::class),
+        ];
     }
 }
